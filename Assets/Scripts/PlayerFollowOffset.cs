29854acebb689d6bb7432a39967e4a1d97ea424a
Guilherme de Deus <kingbowserii@gmail.com>
2024-02-12 15:09:47 -0300

    Setting camera to follow player with an offset to mouse position

---
+++ Assets/Scripts/PlayerFollowOffset.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class PlayerFollowOffset : MonoBehaviour
+{
+    Camera mainCamera;
+    CinemachineVirtualCamera vCamera;
+    CinemachineTransposer transposer;
+
+    [SerializeField]
+    Player player;
+
+    float baseOffsetZ;
+
+
+    private void Start() {
+        mainCamera = Camera.main;
+
+        vCamera = GetComponent<CinemachineVirtualCamera>();
+        transposer = vCamera.GetCinemachineComponent<CinemachineTransposer>();
+
+        baseOffsetZ = transposer.m_FollowOffset.z;
+    }
+
+    private void Update() {
+        Vector3 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 offsetDirection = (mousePos - player.transform.position).normalized;
+
+        transposer.m_FollowOffset = new Vector3(offsetDirection.x, offsetDirection.y, baseOffsetZ);
+    }
+
+
+}
