7c8762a721c767a9971e224458bb2b3d3a1da59d
AidanGraef31 <aidantgraef@gmail.com>
2024-02-13 13:43:03 -0500

    Enemy Spawner is working

---
+++ Assets/Scripts/Enemy Spawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] int numEnemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DoSpawnEnemy());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator DoSpawnEnemy()
+    {
+        for (int i = 0; i < numEnemies; i++)
+        {
+            Instantiate(enemyPrefab);
+            yield return new WaitForSeconds(Random.Range(2, 8));
+        }
+    }
+}
