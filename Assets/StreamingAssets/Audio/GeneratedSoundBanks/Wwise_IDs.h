ca4171fadc7050e695c6b90162ce9424c56d43b5
KhaoKG <Evanseamans@gmail.com>
2024-02-14 00:27:00 -0800

    Large Wwise Commit! Many Events

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,9 +13,38 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID AMBIENTSTART = 1818308409U;
+        static const AkUniqueID BOSSSTATE = 3011728793U;
+        static const AkUniqueID CREDITSTATE = 924228849U;
+        static const AkUniqueID DOORCLOSED = 1697683977U;
+        static const AkUniqueID DOOROPENED = 2649542654U;
+        static const AkUniqueID DOORVACUUMEVENT = 1046624244U;
+        static const AkUniqueID DOORVACUUMSTOP = 2136737028U;
+        static const AkUniqueID ENEMYSUCKED = 3431269606U;
+        static const AkUniqueID HEALTHDROPPED = 1174275515U;
+        static const AkUniqueID KEYDROPPED = 3986149976U;
+        static const AkUniqueID LEVELONESTATE = 1399705530U;
+        static const AkUniqueID MENUBACK = 2634859593U;
+        static const AkUniqueID MENUFORWARD = 2780352335U;
+        static const AkUniqueID MENUMOVE = 1184221871U;
+        static const AkUniqueID MUSICSTART = 1122283870U;
+        static const AkUniqueID PLAYBUTTON = 1843531235U;
+        static const AkUniqueID PLAYERDIE = 3966601280U;
+        static const AkUniqueID PLAYERHEALTHUP = 546141403U;
+        static const AkUniqueID PLAYERHIT = 3831688773U;
+        static const AkUniqueID PLAYERHURT = 3537581393U;
+        static const AkUniqueID PLAYERKEYGAIN = 904027786U;
+        static const AkUniqueID PLAYERKEYUSED = 1314825040U;
+        static const AkUniqueID PLAYERLASTHIT = 965823281U;
+        static const AkUniqueID PLAYERSWING = 1775438520U;
+        static const AkUniqueID POWERSELECT = 2209591688U;
+        static const AkUniqueID SLIDERCLICK = 1218812578U;
+        static const AkUniqueID SLIDERUNCLICK = 3370401445U;
         static const AkUniqueID TESTFAIL = 1435302941U;
         static const AkUniqueID TESTSTART = 4011858643U;
         static const AkUniqueID TESTSUCCESS = 1423690258U;
+        static const AkUniqueID TITLESTATE = 1179082440U;
+        static const AkUniqueID TRANSITIONSTATE = 471385319U;
     } // namespace EVENTS
 
     namespace STATES
