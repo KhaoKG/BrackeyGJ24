67a6c7792124d5c2c4a28037d416f4785f85b29c
Guilherme de Deus <kingbowserii@gmail.com>
2024-02-14 16:37:58 -0300

    Allowing random parameterized spawns

---
+++ Assets/Scripts/Enums/EnemySpawnEnum.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemySpawnEnum
+{
+    Grunt,
+    Ranged,
+    Tough,
+
+    // Random enemy
+    Random = 10
+}
--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -2,19 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameStateManager : MonoBehaviour
+public class GameStateManager : Singleton<GameStateManager>
 {
+    [SerializeField] Player player;
     [SerializeField] GameObject KeySelectionScreen;
-    [SerializeField] GameObject[] EnemySpawners;
+    [SerializeField] EnemySpawner enemySpawner;
     int currentNumEnemies = 4;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         GameObject[] remainingEnemies = GameObject.FindGameObjectsWithTag("Enemy");
@@ -22,30 +16,28 @@ public class GameStateManager : MonoBehaviour
         {
             // killed all enemies, round is done.
             KeySelectionScreen.SetActive(true);
-            FindObjectOfType<Player>().DisableInput();
-
-
+            player.DisableInput();
         }
     }
 
-    public void KeySelected(string Key)
-    {
-        // TODO: ADD THE KEY WHEREVER IT IS SUPPOSED TO GO
+    //public void KeySelected(string Key)
+    //{
+    //    // TODO: ADD THE KEY WHEREVER IT IS SUPPOSED TO GO
 
-        // Reset the Enemy Spawners
-        currentNumEnemies += 2;
-        foreach(GameObject enemySpawner in EnemySpawners)
-        {
-            enemySpawner.GetComponent<EnemySpawner>().numEnemies = currentNumEnemies;
-            Debug.Log("setting num enemies to spawn to " + enemySpawner.GetComponent<EnemySpawner>().numEnemies);
-            enemySpawner.SetActive(true);
-            enemySpawner.GetComponent<EnemySpawner>().SpawnEnemies();
-        }
+    //    // Reset the Enemy Spawners
+    //    currentNumEnemies += 2;
+    //    foreach(GameObject enemySpawner in enemySpawner)
+    //    {
+    //        enemySpawner.GetComponent<EnemySpawner>().numEnemies = currentNumEnemies;
+    //        Debug.Log("setting num enemies to spawn to " + enemySpawner.GetComponent<EnemySpawner>().numEnemies);
+    //        enemySpawner.SetActive(true);
+    //        enemySpawner.GetComponent<EnemySpawner>().SpawnEnemies();
+    //    }
 
-        // Reset the Key Selection Screen
-        KeySelectionScreen.SetActive(false);
+    //    // Reset the Key Selection Screen
+    //    KeySelectionScreen.SetActive(false);
 
-        // Reactivate player
-        FindObjectOfType<Player>().EnableInput();
-    }
+    //    // Reactivate player
+    //    player.EnableInput();
+    //}
 }
