2ad544354d1b953169c93b2f705298853c82d8e7
Guilherme de Deus <kingbowserii@gmail.com>
2024-02-16 01:57:19 -0300

    Adding influence from player's attack direction

--- Assets/Scripts/PlayerPunch.cs
+++ Assets/Scripts/PlayerPunch.cs
@@ -18,7 +18,11 @@ public class PlayerPunch : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.CompareTag("Enemy")) {
-            Vector2 knockbackDirection = collision.transform.position - transform.position;
+            Vector2 knockbackDirection = (collision.transform.position - transform.position).normalized;
+
+            // Add an influence from the player slam direction
+            knockbackDirection += ((Vector2)(transform.position - player.transform.position)).normalized;
+
             collision.gameObject.GetComponent<Enemy>().TakeDamage(punchDamage, knockbackDirection.normalized);
 
             // Shake camera
