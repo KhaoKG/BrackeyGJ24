8b02cb25b46c50f1e67ded52e23873d06105a181
Guilherme de Deus <kingbowserii@gmail.com>
2024-02-17 22:12:41 -0300

    Fading slowly

--- Assets/Scripts/FadeBottomDoor.cs
+++ Assets/Scripts/FadeBottomDoor.cs
@@ -9,19 +9,28 @@ public class FadeBottomDoor : MonoBehaviour
     [SerializeField]
     float transparentAlpha = 0.4f;
 
+    float targetAlpha = 1f;
+    float fadeSpeed = 2f;
+
     void Start()
     {
         // Get the SpriteRenderer component attached to this wall
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
+    private void Update() {
+        if (spriteRenderer.color.a != targetAlpha) {
+            Color color = spriteRenderer.color;
+            color.a = Mathf.MoveTowards(color.a, targetAlpha, fadeSpeed * Time.deltaTime);
+            spriteRenderer.color = color;
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision) {
         // Check if the object colliding with the wall is the player
         if (collision.CompareTag("Player")) {
             // Lower the alpha value to, for example, 0.5 to make the wall semi-transparent
-            Color color = spriteRenderer.color;
-            color.a = transparentAlpha;
-            spriteRenderer.color = color;
+            targetAlpha = transparentAlpha;
         }
     }
 
@@ -29,9 +38,7 @@ public class FadeBottomDoor : MonoBehaviour
         // Check if the object leaving the wall's collider is the player
         if (collision.CompareTag("Player")) {
             // Reset the alpha value to 1 to make the wall fully opaque
-            Color color = spriteRenderer.color;
-            color.a = 1f;
-            spriteRenderer.color = color;
+            targetAlpha = 1f;
         }
     }
 }
